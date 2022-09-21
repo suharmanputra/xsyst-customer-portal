92ba85b50ea277bebbdbf30bbfe30a8d769514fe
suharmanputra <nau6hty_b0y45@yahoo.co.id>
2022-09-21 14:31:51 +0700

    2 files added

---
+++ src/app/privacypolicy-page/privacypolicy-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-privacypolicy-page',
+  templateUrl: './privacypolicy-page.component.html',
+  styleUrls: ['./privacypolicy-page.component.css'],
+})
+export class PrivacypolicyPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
